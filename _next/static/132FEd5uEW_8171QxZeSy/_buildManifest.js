e01149a5e39f0399ca54eeb0179c8c455f7b502c
tak42 <tak42@users.noreply.github.com>
2022-02-21 14:50:12 +0000

    deploy: 90ec97213704e6d9772f8135f61a553b902a24b8

---
+++ _next/static/132FEd5uEW_8171QxZeSy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/239-7767a238ec99d5e9.js","static/chunks/pages/index-21aff9fa3424dd5f.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/cMVJhI-_zxlIS-jcc3AGQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/239-7767a238ec99d5e9.js","static/chunks/pages/index-a0fe42e8d19feb8b.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
