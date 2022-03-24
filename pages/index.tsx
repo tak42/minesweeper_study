3fd28a163470986e4630ee5914d163649d762460
tak42 <1206ookubo@gmail.com>
2022-03-24 20:40:45 +0900

    初回クリック時の動作を変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useMemo, useState } from 'react'
@@ -87,32 +88,98 @@ const Home: NextPage = () => {
     return [...Array(fc.len)].map(() => [...Array(fc.len + fc.margin)].map(() => false))
   }
   const [field, setField] = useState(fieldClick)
-
-  const shuffle = (x: number, y: number) => {
+  const [bombPosition, setBombPosition] = useState(fieldData)
+  // 初回はボムをクリックさせない
+  // 区域を分けて、ボムの配置を意図的に避けさせる
+  // 初回クリック時の位置からX+-1、Y+-1以外の場所にボムを配置
+  // 例）
+  // 初回クリック位置：x=1,y=2(盤面でいうと左上）x=0~2,y=1~3が安全地帯(0,1)~(2,3)まで
+  // 安全地帯以外の数字を使用して配列を作成
+  // 作成した配列2つから数字の組み合わせをボムの数だけ考える
+  const shuffle = (max: number, min: number) => {
     const fc = fieldComponet
     const arr = [...Array(fc.bomb)].map((elm, idx) => idx)
     let a = arr.length
     while (a) {
-      const j = Math.floor(Math.random() * a)
+      const j = Math.floor(Math.random() * (max - min)) + min
       const t = arr[--a]
       arr[a] = arr[j]
       arr[j] = t
     }
     return arr
   }
+  // 与えられた配列の要素を、同じ要素は返さないようにランダムに取得する関数を返す（高階）関数
+  const randomNextMaker = (ary: number[]) => {
+    // 引数のary のコピーを作成
+    ary = [...ary]
 
+    return () => {
+      // 配列が空になっている場合
+      if (ary.length === 0) return { done: true }
+
+      // 0以上 ary.length-1以下の整数を取得
+      const randomIndex = Math.floor(Math.random() * ary.length)
+
+      // 返却すべき要素を取得
+      const value = ary[randomIndex]
+
+      // randomIndexの位置の要素を除去
+      ary.splice(randomIndex, 1)
+
+      return { value, done: false }
+    }
+  }
+  const firstBombSet = () => {
+    const fc = fieldComponet
+    // movieComb の重複無しランダム取得関数を得る。
+    const nextComb = randomNextMaker([0, 1, 2, 3, 4, 5, 6, 7])
+    for (let i = 0; i < 8; i++) {
+      console.log(nextComb())
+    }
+    const arr1 = [...Array(8)].map((elm, idx) => idx)
+    const arr2 = [...Array(8)].map((elm, idx) => idx)
+    const arrAfter1: number[] = arr1.filter((e) => e < 0 || e > 2)
+    const arrAfter2: number[] = arr2.filter((e) => e < 1 || e > 3)
+    let arrComb = []
+    for (const num of arrAfter1) {
+      for (const num2 of arrAfter2) {
+        arrComb.push([num, num2])
+      }
+    }
+    // シャッフルする
+    arrComb = _.shuffle(arrComb)
+    // 次の組み合わせを得るためのジェネレーターを作成
+    const combGenerator = (function* () {
+      yield* arrComb
+    })()
+    const bombSetFld = fieldData()
+    for (let i = 0; i < 8; i++) {
+      const nextObj = combGenerator.next()
+      const val = nextObj.value
+      if (val === undefined) {
+        continue
+      }
+      console.log(val[0], val[1])
+      bombSetFld[val[0]][val[1]] = 99
+      for (const direction of directions) {
+        const newX = val[0] + direction[0] * 1
+        const newY = val[1] + direction[1] * 1
+        if (newX < 0 || newY < 0 || newX > fc.len - 1 || newY > fc.len + fc.margin - 1) break
+        if (bombSetFld[newX][newY] != 99) bombSetFld[newX][newY] += 1
+      }
+    }
+    setBombPosition(bombSetFld)
+  }
   // prettier-ignore
   const directions: number[][] = [
     [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]
   ]
-  // 初回はボムをクリックさせない
-  // 区域を分けて、ボムの配置を意図的に避けさせる
-  // 初回クリック時の位置からX+2、Y+2以外の場所にボムを配置
 
   const bombSet = (x: number, y: number) => {
     const fc = fieldComponet
-    const a1 = shuffle(x, y)
-    const a2 = shuffle(x, y)
+    const a1 = shuffle(x + 1, x - 1)
+    const a2 = shuffle(y + 1, y - 1)
+    console.log(a1, a2)
     const bombSetFld = fieldData()
     for (let i = 0; i < fc.len; i++) {
       const x = a1[i]
@@ -127,7 +194,6 @@ const Home: NextPage = () => {
     }
     return bombSetFld
   }
-  const [bombPosition, setBombPosition] = useState(fieldData)
 
   const displayData = (x: number, y: number) => {
     const isPanelOpen = field[x][y]
@@ -162,9 +228,14 @@ const Home: NextPage = () => {
     return field
   }
   const [isBegin, setIsBegin] = useState(false)
-  const openPanel = (x: number, y: number) => {
+
+  const openPanel = (x: number, y: number, isBegin: boolean) => {
+    console.log(isBegin)
     if (!isBegin) {
-      setBombPosition(bombSet(x, y))
+      firstBombSet()
+      // setBombPosition(bombSet(x, y))
+      setIsBegin(true)
+      // return false
     }
     let newField = JSON.parse(JSON.stringify(field))
     const bombN = bombPosition[x][y]
@@ -175,7 +246,7 @@ const Home: NextPage = () => {
 
   const clear = () => {
     setField(fieldClick())
-    setBombPosition(bombSet)
+    // setBombPosition(bombSet)
   }
 
   return (
@@ -192,7 +263,11 @@ const Home: NextPage = () => {
         <Grid>
           {field.map((row, x) =>
             row.map((col, y) => (
-              <Area key={`${x}-${y}`} clicked={field[x][y]} onClick={() => openPanel(x, y)}>
+              <Area
+                key={`${x}-${y}`}
+                clicked={field[x][y]}
+                onClick={() => openPanel(x, y, isBegin)}
+              >
                 {displayData(x, y)}
               </Area>
             ))
