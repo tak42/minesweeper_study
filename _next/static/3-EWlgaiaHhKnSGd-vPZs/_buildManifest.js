ef20e0829f79d02ea8337e7323781ff05c69696b
tak42 <tak42@users.noreply.github.com>
2022-03-26 14:50:32 +0000

    deploy: 8018738b171e42af87173e81a4157bbb7fd4a866

--- _next/static/3-EWlgaiaHhKnSGd-vPZs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-fbcfe2172188e46f.js","static/chunks/239-e15637892b45c534.js","static/chunks/pages/index-6230e6542dc0bc46.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
