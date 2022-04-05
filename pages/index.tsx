7e0d6147a5da2bfea05fcfbfa7955ffe31c0568e
tak42 <1206ookubo@gmail.com>
2022-04-05 22:06:46 +0900

    ボムマーカーのカウントアップ処理不具合修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,6 +70,8 @@ const Logo = styled.span`
 const Home: NextPage = () => {
   const [level, setLevel] = useState(0)
 
+  type cell = [number, number]
+
   const fieldComponet = useMemo(() => {
     return {
       len: level < 1 ? 8 : 14,
@@ -88,50 +90,23 @@ const Home: NextPage = () => {
     return [...Array(fc.len)].map(() => [...Array(fc.len + fc.margin)].map(() => false))
   }
   const [field, setField] = useState(fieldClick)
+
   const [bombPosition, setBombPosition] = useState(fieldData)
-  const shuffle = (max: number, min: number) => {
-    const fc = fieldComponet
-    const arr = [...Array(fc.bomb)].map((elm, idx) => idx)
-    let a = arr.length
-    while (a) {
-      const j = Math.floor(Math.random() * (max - min)) + min
-      const t = arr[--a]
-      arr[a] = arr[j]
-      arr[j] = t
-    }
-    return arr
-  }
-  // 与えられた配列の要素を、同じ要素は返さないようにランダムに取得する関数を返す（高階）関数
-  // const randomNextMaker = (ary: number[]) => {
-  //   // 引数のary のコピーを作成
-  //   ary = [...ary]
-  //   return () => {
-  //     // 配列が空になっている場合
-  //     if (ary.length === 0) return { done: true }
-  //     // 0以上 ary.length-1以下の整数を取得
-  //     const randomIndex = Math.floor(Math.random() * ary.length)
-  //     // 返却すべき要素を取得
-  //     const value = ary[randomIndex]
-  //     // randomIndexの位置の要素を除去
-  //     ary.splice(randomIndex, 1)
-  //     return { value, done: false }
-  //   }
-  // }
+
   // prettier-ignore
   const directions: number[][] = [
     [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]
   ]
-  const firstBombSet = () => {
+  const firstBombSet = (cell: cell) => {
     const fc = fieldComponet
-    // movieComb の重複無しランダム取得関数を得る。
-    // const nextComb = randomNextMaker([0, 1, 2, 3, 4, 5, 6, 7])
-    // for (let i = 0; i < 8; i++) {
-    //   console.log(nextComb())
-    // }
     const arr1 = [...Array(8)].map((elm, idx) => idx)
     const arr2 = [...Array(8)].map((elm, idx) => idx)
-    const arrAfter1: number[] = arr1.filter((e) => e < 0 || e > 2)
-    const arrAfter2: number[] = arr2.filter((e) => e < 1 || e > 3)
+    const minusX = cell[0] - 1 < 0 ? 0 : cell[0] - 1
+    const plusX = cell[0] + 1 > fc.len ? fc.len : cell[0] + 1
+    const minusY = cell[1] - 1 < 0 ? 0 : cell[1] - 1
+    const plusY = cell[1] + 1 > fc.len ? fc.len : cell[1] + 1
+    const arrAfter1: number[] = arr1.filter((e) => e < minusX || e > plusX)
+    const arrAfter2: number[] = arr2.filter((e) => e < minusY || e > plusY)
     let arrComb: number[][] = []
     for (const num of arrAfter1) {
       for (const num2 of arrAfter2) {
@@ -145,16 +120,14 @@ const Home: NextPage = () => {
       yield* arrComb
     })()
     const bombSetFld = fieldData()
-    const combList = [...Array(8)].map((elm) => combGenerator.next().value)
+    const combList = [...Array(8)].map(() => combGenerator.next().value)
     for (const val of combList) {
       if (val === undefined) continue
       bombSetFld[val[0]][val[1]] = 99
       for (const direction of directions) {
         const newX = val[0] + direction[0] * 1
         const newY = val[1] + direction[1] * 1
-        // 底辺に爆弾が存在する場合、周辺のマスへのカウントアップ処理が正常に行われていない
-        if (val[0] === 7) console.log(newX, newY)
-        if (newX < 0 || newY < 0 || newX > fc.len - 1 || newY > fc.len + fc.margin - 1) break
+        if (newX < 0 || newY < 0 || newX > fc.len - 1 || newY > fc.len + fc.margin - 1) continue
         if (bombSetFld[newX][newY] != 99) bombSetFld[newX][newY] += 1
       }
     }
@@ -171,9 +144,11 @@ const Home: NextPage = () => {
     return data
   }
 
-  const revealCells = (field: boolean[][], bombField: number[][], x: number, y: number) => {
+  const revealCells = (field: boolean[][], bombField: number[][], cell: cell) => {
     const fc = fieldComponet
     const side = fc.len + fc.margin
+    const x = cell[0]
+    const y = cell[1]
     field[x][y] = true
     if (bombField[x][y] > 0) return field
     for (const direction of directions) {
@@ -181,36 +156,36 @@ const Home: NextPage = () => {
       const newY = y + direction[1] * 1
       if (newX < 0 || newY < 0 || newX > fc.len - 1 || newY > side - 1) continue
       if (field[newX][newY] === true) continue
-      revealCells(field, bombField, newX, newY)
+      revealCells(field, bombField, [newX, newY])
     }
     return field
   }
   const [isBegin, setIsBegin] = useState(false)
-  const beginCheck = (isBegin: boolean) => {
+  const beginCheck = (isBegin: boolean, cell: cell) => {
     return new Promise<number[][]>((resolve, reject) => {
       if (!isBegin) {
-        const bombField = firstBombSet()
+        const bombField = firstBombSet(cell)
         setIsBegin(true)
         return resolve(bombField)
       }
       return resolve(bombPosition)
     })
   }
-  const openPanel = (x: number, y: number, isBegin: boolean) => {
-    beginCheck(isBegin).then((check) => {
+  const openPanel = (cell: cell, isBegin: boolean) => {
+    beginCheck(isBegin, cell).then((check) => {
       // bombPositionの処理が間に合っていないため初回クリック時にすべてのマスがオープンしてしまう
       let newField = JSON.parse(JSON.stringify(field))
       const bombField = check
       setBombPosition(bombField)
-      if (bombField[x][y] === 99) console.log('負け')
-      newField = revealCells(newField, bombField, x, y)
+      if (bombField[cell[0]][cell[1]] === 99) console.log('負け')
+      newField = revealCells(newField, bombField, cell)
       setField(newField)
     })
   }
 
   const clear = () => {
     setField(fieldClick())
-    setBombPosition(firstBombSet())
+    setIsBegin(false)
   }
 
   return (
@@ -229,7 +204,7 @@ const Home: NextPage = () => {
               <Area
                 key={`${x}-${y}`}
                 clicked={field[x][y]}
-                onClick={() => openPanel(x, y, isBegin)}
+                onClick={() => openPanel([x, y], isBegin)}
               >
                 {displayData(x, y)}
               </Area>
